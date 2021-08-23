ebf4c3a46a19929a882525d58b1b03cc87187150
Sami Barasi <sami@barasi.gmbh>
2021-08-23 02:37:52 +0200

    keylogger added

---
+++ src/keycontrol.py
@@ -0,0 +1,34 @@
+from pynput import keyboard
+import time
+
+timestamp = time.time()
+keymemo = []
+
+print("Timestamp: {}".format(timestamp))
+print("Timestamp: {}".format(int(timestamp)))
+
+def checkMemo(str):
+    print(str)
+    switcher = {
+        "abc": "ABC",
+        "cde": "CDE"
+    }
+    return switcher.get(str, "")
+
+# The event listener will be running in this block
+with keyboard.Events() as events:
+    for event in events:
+        if timestamp + 5 < time.time():
+            keymemo.clear()
+        if isinstance(event, keyboard.Events.Press):
+            if event.key == keyboard.Key.esc:
+                break
+            else:
+                try:
+                    keymemo.append(event.key.char)
+                except AttributeError:
+                    pass
+                print(checkMemo("".join(str(i) for i in keymemo)))
+                timestamp = time.time()
+                print('Received event {}'.format(event))
+                print('KeyMemo {}'.format(keymemo))
